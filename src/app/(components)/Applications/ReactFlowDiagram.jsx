adb85bb7732f9b8b137ea9b384c36ec42a3de740
Ameera Jayakumar <ameeraj.mec@gmail.com>
2024-08-14 13:36:02 +0530

    react flow diagram with sub flow

--- src/app/(components)/Applications/ReactFlowDiagram.jsx
+++ src/app/(components)/Applications/ReactFlowDiagram.jsx
@@ -1,31 +1,25 @@
 import { useCallback } from 'react';
-import { ReactFlow, addEdge, Background, useNodesState, useEdgesState, MiniMap, Controls } from '@xyflow/react';
+import { ReactFlow, addEdge, Background, useNodesState, useEdgesState, Controls } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 const initialNodes = [
   {
     id: '1',
     type: 'input',
-    data: { label: 'Node 0' },
+    data: { label: 'Start Node' },
     position: { x: 250, y: 5 },
     className: 'light',
   },
   {
     id: '2',
-    data: { label: 'Group A' },
+    data: { label: 'Left Node' },
     position: { x: 100, y: 100 },
     className: 'light',
-    style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 200, height: 200 },
   },
-  {
-    id: '2a',
-    data: { label: 'Node A.1' },
-    position: { x: 10, y: 50 },
-    parentId: '2',
-  },
+
   {
     id: '3',
-    data: { label: 'Node 1' },
+    data: { label: 'Right Node' },
     position: { x: 320, y: 100 },
     className: 'light',
   },
@@ -34,53 +28,42 @@ const initialNodes = [
     data: { label: 'Group B' },
     position: { x: 320, y: 200 },
     className: 'light',
-    style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 300, height: 300 },
+    style: { backgroundColor: 'rgb(242,242,243,0.3)', width: 350, height: 300 },
     type: 'group',
   },
   {
     id: '4a',
-    data: { label: 'Node B.1' },
-    position: { x: 15, y: 65 },
+    data: { label: 'Sub Start Node' },
+    position: { x: 75, y: 30 },
+    className: 'light',
+    parentId: '4',
+    extent: 'parent',
+  },
+
+  {
+    id: '4b',
+    data: { label: 'Sub Left Node' },
+    position: { x: 15, y: 100 },
     className: 'light',
     parentId: '4',
     extent: 'parent',
   },
   {
-    id: '4b',
-    data: { label: 'Group B.A' },
-    position: { x: 15, y: 120 },
+    id: '4c',
+    data: { label: 'Sub Right Node' },
+    position: { x: 180, y: 170 },
     className: 'light',
-    style: {
-      backgroundColor: 'rgba(255, 0, 255, 0.2)',
-      height: 150,
-      width: 270,
-    },
     parentId: '4',
-  },
-  {
-    id: '4b1',
-    data: { label: 'Node B.A.1' },
-    position: { x: 20, y: 40 },
-    className: 'light',
-    parentId: '4b',
-  },
-  {
-    id: '4b2',
-    data: { label: 'Node B.A.2' },
-    position: { x: 100, y: 100 },
-    className: 'light',
-    parentId: '4b',
+    extent: 'parent',
   },
 ];
 
 const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e1-3', source: '1', target: '3' },
-  { id: 'e2a-4a', source: '2a', target: '4a' },
-  { id: 'e3-4b', source: '3', target: '4b' },
-  { id: 'e4a-4b1', source: '4a', target: '4b1' },
-  { id: 'e4a-4b2', source: '4a', target: '4b2' },
-  { id: 'e4b1-4b2', source: '4b1', target: '4b2' },
+  { id: 'e1', source: '1', target: '2', animated: true },
+  { id: 'e2', source: '1', target: '3' },
+  { id: 'e3', source: '3', target: '4a' },
+  { id: 'e4', source: '4a', target: '4b' },
+  { id: 'e5', source: '4a', target: '4c' },
 ];
 
 const ReactFlowDiagram = () => {
@@ -101,7 +84,6 @@ const ReactFlowDiagram = () => {
       className="react-flow-subflows-example"
       fitView
     >
-      <MiniMap />
       <Controls />
       <Background />
     </ReactFlow>
